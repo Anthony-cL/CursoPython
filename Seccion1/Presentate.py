821b0b2fe66e18b678264b881cf8fd5fdf096586
Anthony <anthony.torres.p@gmail.com>
2024-11-29 19:38:48 -0300

    primeros pasos

---
+++ Seccion1/Presentate.py
@@ -0,0 +1,11 @@
+# Creo diferentes varianbles con informacion basica de mi
+
+nombre = "Anthony"
+edad = 29
+pais = "Chile"
+
+# Utilizo la palabra reservada Print para mostrar mis variables en una frease.
+
+print(f"My nombre es {nombre} tengo {edad} años y soy de {pais}")
+
+print(type(edad))
